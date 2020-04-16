7216e04139fb82e73bbbdc3bf1b61bf2e38abde5
devwalex <salamiusmanolawale@gmail.com>
2020-04-16 12:27:37 +0100

    Reusable Form Components: BaseInput

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -6,31 +6,28 @@
         <option v-for="cat in categories" :key="cat">{{ cat }}</option>
       </select>
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
-      </div>
+      <BaseInput
+        type="text"
+        placeholder="Title"
+        label="Title"
+        v-model="event.title"
+        class="field"
+      />
+      <BaseInput
+        type="text"
+        placeholder="Description"
+        label="Description"
+        v-model="event.description"
+        class="field"
+      />
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
-      </div>
+      <BaseInput
+        type="text"
+        placeholder="Add a location"
+        label="Location"
+        v-model="event.location"
+        class="field"
+      />
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
