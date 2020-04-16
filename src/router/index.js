57a0053c02260ed21157a2b1e3bcba49251344de
devwalex <salamiusmanolawale@gmail.com>
2020-04-16 11:40:55 +0100

    Error Handling

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,8 @@ import EventList from '../views/EventList.vue'
 import EventShow from '../views/EventShow.vue'
 import NProgress from 'nprogress'
 import store from '@/store/index.js'
-
+import NotFound from '../views/NotFound.vue'
+import NetworkIssue from '../views/NetworkIssue.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -21,16 +22,40 @@ const routes = [
     component: EventShow,
     props: true,
     beforeEnter(routeTo, routeFrom, next) {
-      store.dispatch('event/fetchEvent', routeTo.params.id).then(event => {
-        routeTo.params.event = event
-        next()
-      })
+      store
+        .dispatch('event/fetchEvent', routeTo.params.id)
+        .then(event => {
+          routeTo.params.event = event
+          next()
+        })
+        .catch(error => {
+          if (error.response && error.response.status == 404) {
+            next({ name: '404', params: { resource: 'event' } })
+          } else {
+            next({ name: 'network-issue' })
+          }
+        })
     }
   },
   {
     path: '/event-create',
     name: 'event-create',
     component: EventCreate
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: NotFound,
+    props: true
+  },
+  {
+    path: '/network-issue',
+    name: 'network-issue',
+    component: NetworkIssue
+  },
+  {
+    path: '*',
+    redirect: { name: '404', params: { resource: 'page' } }
   }
 ]
 
--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -60,25 +60,16 @@ export default {
           dispatch('notification/add', notification, { root: true })
         })
     },
-    fetchEvent({ commit, getters, dispatch }, id) {
+    fetchEvent({ commit, getters }, id) {
       let event = getters.getEventById(id)
       if (event) {
         commit('SET_EVENT', event)
         return event
       } else {
-        return EventService.getEvent(id)
-          .then(response => {
-            commit('SET_EVENT', response.data)
-            return response.data
-          })
-          .catch(error => {
-            const notification = {
-              type: 'error',
-              message:
-                'There was an error while fetching event: ' + error.message
-            }
-            dispatch('notification/add', notification, { root: true })
-          })
+        return EventService.getEvent(id).then(response => {
+          commit('SET_EVENT', response.data)
+          return response.data
+        })
       }
     }
   },
